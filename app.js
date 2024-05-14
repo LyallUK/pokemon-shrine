c12affea6760eb973100eab5b311e2b28478fbdc
David Lyall <lyalluk@outlook.com>
2024-05-14 15:58:16 +0100

    added basic search functionality

--- app.js
+++ app.js
@@ -29,58 +29,72 @@ function applyFadeInEffect(element, delay) {
 	}, 20 * delay);
 }
 
-async function displayPokemonData() {
+async function createPokemonData(i) {
+	const pokemonObject = await getPokemonData(i);
+	const rootStyles = getComputedStyle(document.documentElement);
+	const pokemonContainer = document.getElementById("pokemon-container");
+
+	const pokemonDiv = document.createElement("div");
+	pokemonDiv.id = pokemonObject.name;
+	pokemonDiv.classList = "pokemon-tile";
+
+	const typeColor = rootStyles.getPropertyValue(`--${pokemonObject.type[0]}`);
+	pokemonDiv.style.background = typeColor;
+
+	const pokemonID = document.createElement("span");
+	pokemonID.classList = "pokemon-number";
+	pokemonID.innerText = `#${pokemonObject.id}`;
+
+	const pokemonName = document.createElement("span");
+	pokemonName.classList = "pokemon-name";
+	pokemonName.innerText = pokemonObject.name;
+
+	const pokemonGen = document.createElement("span");
+	pokemonGen.classList = "pokemon-gen";
+	pokemonGen.innerText = pokemonObject.generation.name.replace("generation-", "");
+
+	const pokemonSprite = document.createElement("img");
+	pokemonSprite.classList = "pokemon-sprite";
+	pokemonSprite.src = pokemonObject.sprite;
+
+	const pokemonTypes = document.createElement("div");
+	pokemonTypes.classList = "pokemon-types";
+
+	pokemonObject.type.forEach((typeName) => {
+		const pokemonType = document.createElement("span");
+		pokemonType.classList.add("pokemon-type", `${typeName.toLowerCase()}`);
+		pokemonType.innerText = typeName;
+		pokemonTypes.appendChild(pokemonType);
+	});
+
+	// Append elements to DOM
+	pokemonDiv.appendChild(pokemonID);
+	pokemonDiv.appendChild(pokemonName);
+	pokemonDiv.appendChild(pokemonGen);
+	pokemonDiv.appendChild(pokemonTypes);
+	pokemonDiv.appendChild(pokemonSprite);
+	pokemonContainer.appendChild(pokemonDiv);
+
+	// Apply fade-in effect
+	applyFadeInEffect(pokemonDiv, i);
+}
+
+async function displayPokemonData(idOrName) {
 	const displayAmount = 100;
 
-	for (let i = 1; i < displayAmount + 1; i++) {
+	if (!idOrName) {
+		for (let i = 1; i < displayAmount + 1; i++) {
+			try {
+				await createPokemonData(i);
+			} catch (error) {
+				console.error("Error displaying Pokemon:", error);
+			}
+		}
+	} else {
+		// Fetch and display data for the searched Pokémon
 		try {
-			const pokemonObject = await getPokemonData(i);
-			const rootStyles = getComputedStyle(document.documentElement);
-			const pokemonContainer = document.getElementById("pokemon-container");
-
-			const pokemonDiv = document.createElement("div");
-			pokemonDiv.id = pokemonObject.name;
-			pokemonDiv.classList = "pokemon-tile";
-
-			const typeColor = rootStyles.getPropertyValue(`--${pokemonObject.type[0]}`);
-			pokemonDiv.style.background = typeColor;
-
-			const pokemonID = document.createElement("span");
-			pokemonID.classList = "pokemon-number";
-			pokemonID.innerText = `#${pokemonObject.id}`;
-
-			const pokemonName = document.createElement("span");
-			pokemonName.classList = "pokemon-name";
-			pokemonName.innerText = pokemonObject.name;
-
-			const pokemonGen = document.createElement("span");
-			pokemonGen.classList = "pokemon-gen";
-			pokemonGen.innerText = pokemonObject.generation.name.replace("generation-", "");
-
-			const pokemonSprite = document.createElement("img");
-			pokemonSprite.classList = "pokemon-sprite";
-			pokemonSprite.src = pokemonObject.sprite;
-
-			const pokemonTypes = document.createElement("div");
-			pokemonTypes.classList = "pokemon-types";
-
-			pokemonObject.type.forEach((typeName) => {
-				const pokemonType = document.createElement("span");
-				pokemonType.classList.add("pokemon-type", `${typeName.toLowerCase()}`);
-				pokemonType.innerText = typeName;
-				pokemonTypes.appendChild(pokemonType);
-			});
-
-			// Append elements to DOM
-			pokemonDiv.appendChild(pokemonID);
-			pokemonDiv.appendChild(pokemonName);
-			pokemonDiv.appendChild(pokemonGen);
-			pokemonDiv.appendChild(pokemonTypes);
-			pokemonDiv.appendChild(pokemonSprite);
-			pokemonContainer.appendChild(pokemonDiv);
-
-			// Apply fade-in effect
-			applyFadeInEffect(pokemonDiv, i);
+			const pokemon = await getPokemonData(idOrName);
+			await createPokemonData(pokemon.id);
 		} catch (error) {
 			console.error("Error displaying Pokemon:", error);
 		}
@@ -88,3 +102,32 @@ async function displayPokemonData() {
 }
 
 displayPokemonData();
+
+const searchBar = document.getElementById("search-bar");
+
+function handleSearchBar(event) {
+	if (searchBar.value == Number.parseInt(searchBar.value)) {
+		console.log("Its a number");
+	} else console.log("Its a word");
+}
+
+searchBar.addEventListener("keydown", async function (event) {
+	if (event.key === "Enter") {
+		const searchValue = event.target.value.trim().toLowerCase();
+		const pokemonContainer = document.getElementById("pokemon-container");
+
+		pokemonContainer.innerHTML = "";
+
+		if (searchBar.value === "") {
+			displayPokemonData();
+		} else {
+			try {
+				const pokemon = await getPokemonData(searchValue);
+				await createPokemonData(pokemon.id);
+			} catch (error) {
+				console.error("Error displaying Pokemon:", error);
+				pokemonContainer.innerHTML = `<p class="error">Pokémon not found!</p>`;
+			}
+		}
+	}
+});
